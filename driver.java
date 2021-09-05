5ecfb57ad2ec2214ab82150eba21bc678743d548
Brandon Ehrmanntraut <34557284+behrmanntraut@users.noreply.github.com>
2021-09-05 19:18:01 -0400

    Update driver.java
    Now runs through all the needed phases to compile a bare bones empty file into java.

--- driver.java
+++ driver.java
@@ -2,9 +2,20 @@
 *Controls the many other files that this compiler consists of
 *@author Brandon Ehrmanntraut
 */
+import java.util.ArrayList;
 public class driver{
 	public static void main(String[] args){
 		token test = new token("Example.txt");
-		test.testRun();
+		String[] tokens = test.run();
+		ArrayList<String> symbols = test.getSymbolTable();
+		parse parser = new parse(tokens);
+		Boolean valid = parser.run();
+		if(valid){
+			generator gen = new generator(tokens, symbols);
+			gen.run();
+		}else{
+			System.out.println("Code failed to compiler");
+		}
+		
 	}
 }
