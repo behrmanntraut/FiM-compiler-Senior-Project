f972d299ace0b4dc6213623c74b3e8364dfe9c8a
behrmanntraut <brandonehrmanntraut18@gmail.com>
2021-11-11 11:58:27 -0500

    Flushed out malformed sentence errors

--- driver.java
+++ driver.java
@@ -12,7 +12,7 @@ public class driver{
 		ArrayList<String> symbols = test.getSymbolTable();
 		ArrayList<String> t = new ArrayList<String>(Arrays.asList(tokens));
 		ArrayList<Method> methods = test.getMethods();
-		System.out.println(t);
+		//System.out.println(t);
 		//System.out.println("\n\n" + symbols);
 		if(errors.shouldThrowErrors()){
 			try{
